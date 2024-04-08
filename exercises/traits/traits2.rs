d75ca78402e62892f2fcd4486af2eb776a7eb84a
Oveln <oveln@outlook.com>
2024-04-08 14:20:55 +0800

    update

--- exercises/traits/traits2.rs
+++ exercises/traits/traits2.rs
@@ -8,13 +8,21 @@
 //
 // Execute `rustlings hint traits2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+use std::ops::Add;
 
 trait AppendBar {
     fn append_bar(self) -> Self;
 }
 
 // TODO: Implement trait `AppendBar` for a vector of strings.
+impl AppendBar for Vec<String> {
+    fn append_bar(self) -> Self {
+        //        self.iter().map(|s| s.to_owned() + "Bar").collect()
+        let mut ret = self;
+        ret.push("Bar".to_string());
+        ret
+    }
+}
 
 #[cfg(test)]
 mod tests {
